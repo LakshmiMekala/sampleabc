85de741667b7a85f0e9916d45ac4f13bad4eada0
Lakshmi Narayana Mekala <lmekala@tibco.com>
2018-01-11 11:06:58 +0530

    add build file for rest gw

---
+++ recipes/rest-conditional-gateway/rest-conditional-gateway.sh
@@ -0,0 +1,28 @@
+function get_test_cases {
+    local my_list=( testcase1 testcase2 )
+    echo "${my_list[@]}"
+}
+function testcase1 {
+./rest-conditional-gateway &
+pId=$!
+response=$(curl --request GET http://localhost:9096/pets/24 --write-out '%{http_code}' --silent --output /dev/null)
+Kill -9 $pId
+if [ $response -eq 200  ] 
+    then 
+        echo "PASS"
+    else
+        echo "FAIL"
+fi
+}
+function testcase2 {
+./rest-conditional-gateway &
+pId=$!
+response=$(curl --request GET http://localhost:9096/pets/24 --write-out '%{http_code}' --silent --output /dev/null)
+Kill -9 $pId
+if [ $response -eq 200  ] 
+    then 
+        echo "PASS"
+    else
+        echo "FAIL"
+fi
+}
