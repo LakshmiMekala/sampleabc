4ef6bd5d6d03f953d2345bd1c2b19509a27ff651
Lakshmi Narayana Mekala <lmekala@tibco.com>
2018-01-11 15:41:03 +0530

    adding build file for kafka

---
+++ recipes/KafkaTrigger-To-KafkaPublisher/KafkaTrigger-To-KafkaPublisher.sh
@@ -0,0 +1,61 @@
+function get_test_cases {
+    local my_list=( testcase1 )
+    echo "${my_list[@]}"
+}
+function testcase1 {
+
+    pushd $GOPATH/kafka
+    
+    bin/windows/zookeeper-server-start.bat config/zookeeper.properties &
+    pId=$!
+    #echo "$pId"
+    sleep 10
+
+    bin/windows/kafka-server-start.bat config/server.properties &
+    pId1=$!
+    #echo "$pId"
+    sleep 10
+
+    bin/windows/kafka-topics.bat --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic publishpet &
+    pId2=$!
+    sleep 10
+
+    popd
+    ./KafkaTrigger-To-KafkaPublisher &
+    pId4=$!
+    sleep 20
+
+    cd ..
+    cd kafka
+    current_time=$(date "+%Y.%m.%d-%H.%M.%S")
+    #echo check now
+    echo "{\"country\":\"USA\",\"Current Time\" :\"$current_time\"}" | bin/windows/kafka-console-producer.bat --broker-list localhost:9092 --topic publishpet
+    #bin/windows/kafka-console-producer.bat --broker-list localhost:9092 --topic syslog   --property "parse.key=true"  --property "key.separator=:"  key1:USA &
+    pId3=$!
+
+    sleep 2
+    
+    var="$(bin/windows/kafka-console-consumer.bat --topic SubscribePet --bootstrap-server localhost:9092 --timeout-ms 9000 --consumer.config $GOPATH/kafka/config/consumer.properties)"
+    
+    pId5=$!
+    #sleep 10
+    #echo VAR=$var
+    #sleep 10
+    #echo var="$var"
+    kill -SIGINT $pId
+    kill -SIGINT $pId1
+    kill -SIGINT $pId5
+    kill -SIGINT $pId3
+    kill -SIGINT $pId4
+    sleep 20
+    #echo "{\"country\":\"USA\",\"Current Time\" :\"$current_time\"}"
+
+    if [ "$var" == "{\"country\":\"USA\",\"Current Time\" :\"$current_time\"}" ] 
+        then 
+            echo "PASS"
+            
+        else
+            echo "FAIL"
+            
+    fi
+}
--- recipes/rest-conditional-gateway/rest-conditional-gateway.sh
+++ recipes/rest-conditional-gateway/rest-conditional-gateway.sh
@@ -19,7 +19,7 @@ fi
 function testcase2 {
 ./rest-conditional-gateway &
 pId=$!
-response=$(curl --request GET http://localhost:9096/pets/24 --write-out '%{http_code}' --silent --output /dev/null)
+response=$(curl -X PUT "http://localhost:9096/pets" -H "accept: application/xml" -H "Content-Type: application/json" -d '{"category":{"id":16,"name":"Animals"},"id":16,"name":"SPARROW","photoUrls":["string"],"status":"sold","tags":[{"id":0,"name":"string"}]}' --write-out '%{http_code}' --silent --output /dev/null)
 kill -9 $pId
 if [ $response -eq 200  ] 
     then 
