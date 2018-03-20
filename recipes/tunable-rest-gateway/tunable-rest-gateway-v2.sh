e87283d4002bd63181b1b2d049e330ad76975ac2
Lakshmi Narayana Mekala <lmekala@tibco.com>
2018-03-20 18:34:26 +0530

    v2 final

--- recipes/tunable-rest-gateway/tunable-rest-gateway-v2.sh
+++ recipes/tunable-rest-gateway/tunable-rest-gateway-v2.sh
@@ -6,12 +6,12 @@ function get_test_cases {
 }
 function testcase1 {
 		export API_CONTEXT=PETS
-$GOPATH/src/github.com/TIBCOSoftware/mashling-gateway/bin/mashling-gateway -config tunable-rest-gateway.json &
+$GOPATH/src/github.com/TIBCOSoftware/mashling-gateway/bin/mashling-gateway -config tunable-rest-gateway.json > /tmp/tunable1.log 2>&1 &
 pId=$!
 sleep 15;
 response=$(curl --request GET http://localhost:9096/id/17 --write-out '%{http_code}' --silent --output /dev/null)
 kill -9 $pId
-if [ $response -eq 200  ] 
+if [ $response -eq 200 ] && [[ "echo $(cat /tmp/tunable1.log)" =~ "completed" ]]
     then 
         echo "PASS"
     else
@@ -20,11 +20,11 @@ fi
 }
 function testcase2 {
 export API_CONTEXT=USERS
-$GOPATH/src/github.com/TIBCOSoftware/mashling-gateway/bin/mashling-gateway -config tunable-rest-gateway.json &
+$GOPATH/src/github.com/TIBCOSoftware/mashling-gateway/bin/mashling-gateway -config tunable-rest-gateway.json> /tmp/tunable2.log 2>&1 &
 pId=$!
 response=$(curl --request GET http://localhost:9096/id/17 --write-out '%{http_code}' --silent --output /dev/null)
 kill -9 $pId
-if [ $response -eq 200  ] 
+if [ $response -eq 200  ] && [[ "echo $(cat /tmp/tunable2.log)" =~ "completed" ]]
     then 
         echo "PASS"
     else
