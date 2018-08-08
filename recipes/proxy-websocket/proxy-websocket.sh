f2e884b73e08b24d6fd110315c7ba9dc3f28e80f
Lakshmi Narayana Mekala <lmekala@tibco.com>
2018-08-08 16:39:39 +0530

    fix grpc errors

--- recipes/proxy-websocket/proxy-websocket.sh
+++ recipes/proxy-websocket/proxy-websocket.sh
@@ -21,8 +21,7 @@ if [[ "echo $(cat /tmp/websocket1.log)" =~ "Completed" ]]
         echo "FAIL"
 fi
 kill -9 $pId $pId1
-ps -a
-kill -SIGINT $pId2 
+kill $(lsof -t -i:8080)
 }
 
 # Run maximum+1 clients
@@ -32,9 +31,10 @@ pId=$!
 sleep 5
 go run main.go -server > /tmp/server2.log 2>&1 & 
 pId1=$!
+sleep 5
 go run main.go -client -name=CLIENT1 -url=ws://localhost:9096/ws > /tmp/client1.log 2>&1 & pId2=$!
 go run main.go -client -name=CLIENT2 -url=ws://localhost:9096/ws > /tmp/client2.log 2>&1 & pId3=$!
-go run main.go -client -name=CLIENT3 -url=ws://localhost:9096/ws > /tmp/client3.log 2>&1 & pId4=$!
+go run main.go -client -name=CLIENT3 -url=ws://localhost:9096/ws > /tmp/client3.log 2>&1 & 
 sleep 5
 if [[ "echo $(cat /tmp/websocket2.log)" =~ "Completed" ]] && [[ "echo $(cat /tmp/websocket2.log)" =~ "can't accept any more connections" ]]
     then 
@@ -42,9 +42,9 @@ if [[ "echo $(cat /tmp/websocket2.log)" =~ "Completed" ]] && [[ "echo $(cat /tmp
     else
         echo "FAIL"
 fi
-kill -9 $pId $pId1
-ps -a
-kill -SIGINT $pId2 $pId3 $pId4
+kill -9 $pId
+kill -SIGINT $pId2 $pId3
+kill $(lsof -t -i:8080)
 }
 
 # Run client without server
@@ -54,7 +54,7 @@ pId=$!
 sleep 5
 go run main.go -client -name=CLIENT1 -url=ws://localhost:9096/ws > /tmp/client1.log 2>&1
 sleep 5
-error="connectex: No connection could be made because the target machine actively refused it"
+error="connection error:dial tcp 127.0.0.1:8080: connect: connection refused"
 if [[ "echo $(cat /tmp/websocket3.log)" =~ "Completed" ]] && [[ "echo $(cat /tmp/websocket3.log)" =~ "$error" ]] && [[ "echo $(cat /tmp/client1.log)" =~ "failed to connect backend url" ]]
     then 
         echo "PASS"
