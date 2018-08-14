0f865f5d46fbe6639cc8308fe6a3f4757c0804ac
nareshkumarthota <nthota@tibco.com>
2018-08-14 16:29:30 +0530

    grpc testcase update

--- recipes/grpc-to-grpc-gateway/grpc-to-grpc-gateway.sh
+++ recipes/grpc-to-grpc-gateway/grpc-to-grpc-gateway.sh
@@ -30,7 +30,7 @@ function testcase1 {
 ./mashling-gateway -c grpc-to-grpc-gateway.json > /tmp/grpc1.log 2>&1 &
 pId2=$!
 sleep 5
-go run main.go -server &
+go run main.go -server > /tmp/server1.log 2>&1 &
 pId=$!
 sleep 5
 go run main.go -client -port 9096 -method pet -param 2 > /tmp/client1.log 2>&1 &
@@ -53,7 +53,7 @@ function testcase2 {
 ./mashling-gateway -c grpc-to-grpc-gateway.json > /tmp/grpc2.log 2>&1 &
 pId2=$!
 sleep 5
-go run main.go -server &
+go run main.go -server > /tmp/server2.log 2>&1 &
 pId=$!
 sleep 5
 go run main.go -client -port 9096 -method user -param user2 > /tmp/client2.log 2>&1 &
