ed097d197383582b688e721405b9476efd1d556e
Lakshmi Narayana Mekala <lmekala@tibco.com>
2018-08-21 13:36:05 +0530

    new grpc test cases

--- recipes/grpc-to-grpc-gateway/grpc-to-grpc-gateway.sh
+++ recipes/grpc-to-grpc-gateway/grpc-to-grpc-gateway.sh
@@ -1,29 +1,8 @@
 #!/bin/bash
 
 function get_test_cases {
-    # init ;
     local my_list=( testcase1 testcase2 )
     echo "${my_list[@]}"
-    # clear ;
-}
-
-function init {
-    go get google.golang.org/grpc
-    mashling-cli create -c grpc-to-grpc-gateway.json -p petstore.proto -N
-    if [[ "$OSTYPE" == "darwin"* ]] ;then
-        mv mashling-custom/mashling-gateway-darwin-amd64 mashling-custom/mashling-gateway
-        cp mashling-custom/mashling-gateway .
-    elif [[ "$OSTYPE" == "msys"* ]] ;then
-        mv mashling-custom/mashling-gateway-windows-amd64.exe mashling-custom/mashling-gateway.exe
-        cp mashling-custom/mashling-gateway.exe .
-    elif [[ "$OSTYPE" == "linux-gnu"* ]] ;then
-        mv mashling-custom/mashling-gateway-linux-amd64 mashling-custom/mashling-gateway
-        cp mashling-custom/mashling-gateway .
-    fi 
-}
-
-function clear {
-    rm -rf mashilng-custom
 }
 
 function testcase1 {
