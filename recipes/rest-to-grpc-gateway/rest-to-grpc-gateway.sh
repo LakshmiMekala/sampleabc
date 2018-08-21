ecd283a15c0f76149292324a6cf393cb88f91baa
nareshkumarthota <nthota@tibco.com>
2018-08-21 15:09:24 +0530

    grpc test cases

--- recipes/rest-to-grpc-gateway/rest-to-grpc-gateway.sh
+++ recipes/rest-to-grpc-gateway/rest-to-grpc-gateway.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
 function get_test_cases {
-    local my_list=( testcase1 testcase2 )
+    local my_list=( testcase1 testcase2 testcase3 testcase4 )
     echo "${my_list[@]}"
 }
 
 function testcase1 {
+mkdir -p $GOPATH/src/rest-to-grpc-gateway/petstore
+protoc -I . petstore.proto --go_out=plugins=grpc:$GOPATH/src/rest-to-grpc-gateway/petstore/    
 ./mashling-gateway -c rest-to-grpc-gateway.json > /tmp/grpc1.log 2>&1 &
 pId2=$!
 sleep 5
 go run main.go -server > /tmp/server1.log 2>&1 &
 pId=$!
 sleep 5
-response=$(curl --request GET http://localhost:9096/pets/PetById/2 --write-out '%{http_code}' --silent --output /dev/null)
+response=$(curl --request GET http://localhost:9096/petstore/PetById?id=2 --write-out '%{http_code}' --silent --output /dev/null)
 kill -9 $pId
 if [ $response -eq 200  ] && [[ "echo $(cat /tmp/grpc1.log)" =~ "Completed" ]]
     then 
@@ -21,19 +23,22 @@ if [ $response -eq 200  ] && [[ "echo $(cat /tmp/grpc1.log)" =~ "Completed" ]]
         echo "FAIL"
 fi        
 kill -9 $pId2
-var=$(ps --ppid $pId)
-pId7=$(echo $var | awk '{print $5}')
-kill -9 $pId7
+# var=$(ps --ppid $pId)
+# pId7=$(echo $var | awk '{print $5}')
+# kill -9 $pId7
+kill -9 $(lsof -t -i:9000)
 }
 
 function testcase2 {
+mkdir -p $GOPATH/src/rest-to-grpc-gateway/petstore
+protoc -I . petstore.proto --go_out=plugins=grpc:$GOPATH/src/rest-to-grpc-gateway/petstore/
 ./mashling-gateway -c rest-to-grpc-gateway.json > /tmp/grpc2.log 2>&1 &
 pId2=$!
 sleep 5
 go run main.go -server > /tmp/server2.log 2>&1 &
 pId=$!
 sleep 5
-response=$(curl --request GET http://localhost:9096/users/UserByName/user2 --write-out '%{http_code}' --silent --output /dev/null)
+response=$(curl --request GET http://localhost:9096/petstore/UserByName?username=user2 --write-out '%{http_code}' --silent --output /dev/null)
 kill -9 $pId
 if [ $response -eq 200  ] && [[ "echo $(cat /tmp/grpc2.log)" =~ "Completed" ]]
     then 
@@ -42,9 +47,10 @@ if [ $response -eq 200  ] && [[ "echo $(cat /tmp/grpc2.log)" =~ "Completed" ]]
         echo "FAIL"
 fi        
 kill -9 $pId2
-var=$(ps --ppid $pId)
-pId7=$(echo $var | awk '{print $5}')
-kill -9 $pId7
+# var=$(ps --ppid $pId)
+# pId7=$(echo $var | awk '{print $5}')
+# kill -9 $pId7
+kill -9 $(lsof -t -i:9000)
 }
 
 function testcase3 {
@@ -54,7 +60,7 @@ sleep 5
 go run main.go -server > /tmp/server3.log 2>&1 &
 pId=$!
 sleep 5
-response=$(curl -X PUT "http://localhost:9096/pets/PetById" -H "accept: application/xml" -H "Content-Type: application/json" -d '{"id":3}' --write-out '%{http_code}' --silent --output /dev/null)
+response=$(curl -X PUT "http://localhost:9096/petstore/PetPUT" -H "accept: application/xml" -H "Content-Type: application/json" -d '{"pet": {"id": 12,"name": "mycat12"}}' --write-out '%{http_code}' --silent --output /dev/null)
 kill -9 $pId
 if [ $response -eq 200  ] && [[ "echo $(cat /tmp/grpc3.log)" =~ "Completed" ]]
     then 
@@ -63,9 +69,10 @@ if [ $response -eq 200  ] && [[ "echo $(cat /tmp/grpc3.log)" =~ "Completed" ]]
         echo "FAIL"
 fi        
 kill -9 $pId2
-var=$(ps --ppid $pId)
-pId7=$(echo $var | awk '{print $5}')
-kill -9 $pId7
+# var=$(ps --ppid $pId)
+# pId7=$(echo $var | awk '{print $5}')
+# kill -9 $pId7
+kill -9 $(lsof -t -i:9000)
 }
 
 
@@ -76,16 +83,20 @@ sleep 5
 go run main.go -server > /tmp/server4.log 2>&1 &
 pId=$!
 sleep 5
-response=$(curl --request GET http://localhost:9096/users/UserByName?username=user3 --write-out '%{http_code}' --silent --output /dev/null)
+curl --request GET http://localhost:9096/petstore/UserByName?username=user30 > /tmp/get4.log 2>&1
+response=$(curl --request GET http://localhost:9096/petstore/UserByName?username=user30 --write-out '%{http_code}' --silent --output /dev/null)
 kill -9 $pId
-if [ $response -eq 200  ] && [[ "echo $(cat /tmp/grpc4.log)" =~ "Completed" ]]
+if [[ "echo $(cat /tmp/get4.log)" =~ "rpc error: code = Unknown desc = User not found" ]] && [[ "echo $(cat /tmp/grpc4.log)" =~ "Completed" ]]
     then 
         echo "PASS"
     else
         echo "FAIL"
 fi        
 kill -9 $pId2
-var=$(ps --ppid $pId)
-pId7=$(echo $var | awk '{print $5}')
-kill -9 $pId7
+# var=$(ps --ppid $pId)
+# echo "$var"
+# pId7=$(echo $var | awk '{print $5}')
+# echo "$pId7"
+# kill -9 $pId7
+kill -9 $(lsof -t -i:9000)
 }
